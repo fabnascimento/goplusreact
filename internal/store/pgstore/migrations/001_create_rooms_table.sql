655b67d3fb920b2944568726d79dfe6eeb2cc047
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2024-08-08 00:01:52 -0400

    feat: add initial migrations

---
+++ internal/store/pgstore/migrations/001_create_rooms_table.sql
@@ -0,0 +1,10 @@
+-- Write your migrate up statements here
+CREATE TABLE IF NOT EXISTS rooms (
+    "id"    uuid    PRIMARY KEY NOT NULL    DEFAULT gen_random_uuid(),
+    "theme" varchar(255)        NOT NULL
+)
+---- create above / drop below ----
+
+-- Write your migrate down statements here. If this migration is irreversible
+-- Then delete the separator line above.
+DROP TABLE IF EXISTS rooms;
